d6e4ca82879507c00442f3f3c950e79b83fe5fbf
William Faria <wmfarinha@gmail.com>
2019-01-22 00:24:48 -0200

    Inclusão de template e components

---
+++ src/about/about.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+
+    <div>
+        <PageHeader name='Sobre' small='nossa empresa' />
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+    </div>
+
+)
