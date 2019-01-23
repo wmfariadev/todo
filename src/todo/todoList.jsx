1e9b9f2c7fa62dd91da480e1df5e925b6b113b45
William Faria <wmfarinha@gmail.com>
2019-01-23 00:15:00 -0200

    Último commit sem Redux

--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
 
-export default props => (
-    <div>
-        <h1>List</h1>
-    </div>
-)
+export default props => {
+
+    const renderRow = () => {
+        const list = props.list || []
+        return list.map(todo => (
+            <tr key={todo._id}>
+                <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
+                <td>
+                    <IconButton
+                        style='success' icon='check' onClick={() => props.handleMarkeAsDone(todo)} hide={todo.done} />
+                    <IconButton
+                        style='warning' icon='undo' onClick={() => props.handleMarkeAsPending(todo)} hide={!todo.done} />
+                    <IconButton
+                        style='danger' icon='trash-o' onClick={() => props.handleRemove(todo)} hide={!todo.done} />
+                </td>
+            </tr>
+        ))
+    }
+
+    return (
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th className='tableActions'>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRow()}
+            </tbody>
+        </table>
+    )
+}
